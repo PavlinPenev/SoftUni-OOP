1ff7d84b1d5dd3c49a5056a9c98561624e221abf
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-11-06 18:30:10 +0200

    Polymorphism - Lab

---
+++ Polymorphism - Lab/MathOperations/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int x, int y)
+            => x + y;
+
+        public double Add(double x, double y, double z)
+            => x + y + z;
+
+        public decimal Add(decimal x, decimal y, decimal z)
+            => x + y + z;
+    }
+}
