d6007e9b357a1926c7cbccb97cde525775030b98
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-11-16 22:35:54 +0200

    Reflection And Attributes - Lab

---
+++ Reflection And Attributes - Lab/Stealer/Spy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, params string[] namesOfFields)
+        {
+            Type typeClass = Type.GetType(nameOfClass);
+            FieldInfo[] fields = typeClass.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            StringBuilder sb = new StringBuilder();
+            Object instanced = Activator.CreateInstance(typeClass);
+            sb.AppendLine($"Class under investigation: {nameOfClass}");
+            foreach (FieldInfo field in fields.Where(f => namesOfFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instanced)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Reflection And Attributes - Lab/Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy currentSpy = new Spy();
+            string result = currentSpy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
