9350b03f2eb576e1c9253b66b883113f6327568f
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-11-16 23:51:59 +0200

    Reflection And Attributes - Lab

---
+++ Reflection And Attributes - Lab/CreateAttribute/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Pesho")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ Reflection And Attributes - Lab/CreateAttribute/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker // 6. Code Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            StringBuilder stringBuilder = new StringBuilder();
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public);
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (var attribute in attributes)    
+                    {
+                        stringBuilder.AppendLine($"{method.Name} is written by {(attribute as AuthorAttribute).Name}");
+                    }
+                }
+            }
+
+            Console.WriteLine(stringBuilder.ToString().TrimEnd());
+        }
+    }
+}
